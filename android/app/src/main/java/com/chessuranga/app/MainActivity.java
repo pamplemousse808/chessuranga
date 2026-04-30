7f410808bee2c0738fc8e0abd33301c68a0ac9f9
Pamplemousse808 <jamiewhyte@gmail.com>
2026-04-30 22:20:21 +0100

    timer 0.1

--- android/app/src/main/java/com/chessuranga/app/MainActivity.java
+++ android/app/src/main/java/com/chessuranga/app/MainActivity.java
@@ -1,5 +1,12 @@
 package com.chessuranga.app;
 
 import com.getcapacitor.BridgeActivity;
+import androidx.activity.EdgeToEdge;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(android.os.Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+    }
+}
