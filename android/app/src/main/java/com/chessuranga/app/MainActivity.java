d76d3f3f20ca05d6f1225173846a0c77a346207f
Pamplemousse808 <jamiewhyte@gmail.com>
2026-04-30 22:42:35 +0100

    app fixes

--- android/app/src/main/java/com/chessuranga/app/MainActivity.java
+++ android/app/src/main/java/com/chessuranga/app/MainActivity.java
@@ -2,11 +2,17 @@ package com.chessuranga.app;
 
 import com.getcapacitor.BridgeActivity;
 import androidx.activity.EdgeToEdge;
+import android.os.Bundle;
 
 public class MainActivity extends BridgeActivity {
     @Override
-    protected void onCreate(android.os.Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+        getWindow().setStatusBarColor(android.graphics.Color.TRANSPARENT);
+        getWindow().getDecorView().setSystemUiVisibility(
+            android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE |
+            android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
     }
 }
